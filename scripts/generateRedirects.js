ad1197c72bbabcfd5366d134fc43c5d6b6005d06
HANS RAJ <155075212+HansrajS1@users.noreply.github.com>
2025-11-04 00:26:32 +0530

    redirect  url

--- scripts/generateRedirects.js
+++ scripts/generateRedirects.js
@@ -1,15 +1,19 @@
 import fs from "fs";
 import path from "path";
 
-const baseUrl = process.env.VITE_BASE_URL || "http://localhost:3000";
+const baseUrl = process.env.VITE_BASE_URL || "localhost:5173";
 
 if (!baseUrl) {
   console.error("Error: VITE_BASE_URL is not set!");
   process.exit(1);
 }
+
 const redirectsContent = `
-/api/*    ${baseUrl}/api/:splat    200
-/*        /index.html    200
+https://auexamweb.netlify.app/*   ${baseUrl}/:splat   301!
+
+/api/*   ${baseUrl}/api/:splat   200
+
+/*   /index.html   200
 `.trim();
 
 const distPath = path.resolve(process.cwd(), "dist");
@@ -18,4 +22,5 @@ if (!fs.existsSync(distPath)) fs.mkdirSync(distPath, { recursive: true });
 
 const redirectsPath = path.join(distPath, "_redirects");
 fs.writeFileSync(redirectsPath, redirectsContent);
+
 console.log(`_redirects generated successfully at ${redirectsPath}`);
